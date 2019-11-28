9016afbdc6b661b391ba309668ac4c588e56c5c8
Adam Lee <adam.lee@students.plymouth.ac.uk>
2019-11-28 14:12:16 +0000

    Added env vars

--- server/index.js
+++ server/index.js
@@ -1,13 +1,14 @@
 var express = require("express");
 var app = express();
 var bodyParser = require('body-parser');
-var routes = require("./routes/router")
+var routes = require("./routes/router");
+require('dotenv').config();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 app.use(routes);
-port = 9000;
+port = process.env.PORT || 3000;
 
 app.listen(port, function() {
   console.log("Server listening on port " + port);
