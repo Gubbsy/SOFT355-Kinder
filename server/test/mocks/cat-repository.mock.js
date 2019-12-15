2b2bbd0c6781b8fd62b199fcb2a752741aa8b8ff
Adam Lee <adam.lee@students.plymouth.ac.uk>
2019-12-15 16:33:21 +0000

    Set-up tests for web sockets

--- server/test/mocks/cat-repository.mock.js
+++ server/test/mocks/cat-repository.mock.js
@@ -131,6 +131,55 @@ class MockCatRepository {
   async addCookie() {
     
   }
+
+  async getTopCats() {
+    return [
+      {
+        _id: "1",
+        catId: "Meow-1", 
+        imageUrl: "https://caturl-2.com",
+        width: 1920,
+        height: 1080,
+        score: 3,
+        voteCookies: []
+      },
+      {
+        _id: "2",
+        catId: "Meow-2", 
+        imageUrl: "https://caturl-2.com",
+        width: 480,
+        height: 480,
+        score: 2,
+        voteCookies: []
+      },
+      {
+        _id: "3",
+        catId: "Meow-3", 
+        imageUrl: "https://caturl-3.com",
+        width: 720,
+        height: 1240,
+        score: 5,
+        voteCookies: []
+      },
+      {
+        _id: "4",
+        catId: "Meow-4", 
+        imageUrl: "https://caturl-4.com",
+        width: 1020,
+        height: 720,
+        score: 6,
+        voteCookies: []
+      },
+      {
+        _id: "5",
+        catId: "Meow-5", 
+        imageUrl: "https://caturl-5.com",
+        width: 920,
+        height: 640,
+        score: 0,
+        voteCookies: []
+      }]
+  }
 }
 
 module.exports = MockCatRepository
